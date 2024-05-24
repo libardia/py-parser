e9a1466f88d29c0cdaef81b473c6dc7beada195d
Anthony Libardi <libardi42@gmail.com>
2024-05-24 14:33:47 -0400

    use finalise in main tests

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
-from functionalparser import parse_int, chain, ignore_whitespace as iw
+from functionalparser import parse_int, chain, ignore_whitespace as iw, finalize, IgnoreWhitespaceType
 
 
 def main() -> None:
     print(f'{parse_int('test')=}')
     print(f'{parse_int('896847 stuff')=}')
-    two_ints = chain(
-        iw(parse_int), iw(parse_int)
-    )
-    print(f'{two_ints('00034 230')=}')
+    two_ints = finalize(chain(
+        iw(parse_int), iw(parse_int, IgnoreWhitespaceType.AROUND)
+    ))
+    print(f'{two_ints(' 00034 230 ')=}')
 
 
 if __name__ == '__main__':
