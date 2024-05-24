ccd4e3263b3369ec5368d0d3a380ee61896778ed
Anthony Libardi <libardi42@gmail.com>
2024-05-24 14:43:34 -0400

    partial

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
+from functools import partial
+
 from functionalparser import parse_int, chain, ignore_whitespace as iw, finalize, IgnoreWhitespaceType
 
 
 def main() -> None:
+    iw_around = partial(iw, ignore_whitespace_type=IgnoreWhitespaceType.AROUND)
+
     print(f'{parse_int('test')=}')
     print(f'{parse_int('896847 stuff')=}')
     two_ints = finalize(chain(
-        iw(parse_int), iw(parse_int, IgnoreWhitespaceType.AROUND)
+        iw(parse_int), iw_around(parse_int)
     ))
     print(f'{two_ints(' 00034 230 ')=}')
 
