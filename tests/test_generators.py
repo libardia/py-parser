fa8bf9327d4cd2645cdb0cdaab5ee0d77dbbaa47
Anthony Libardi <libardi42@gmail.com>
2024-05-27 18:28:31 -0400

    stubs for all other tests

---
+++ tests/test_generators.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestGenerators(TestCase):
+    def test_take_n(self):
+        self.fail()
+
+    def test_get_char_in(self):
+        self.fail()
+
+    def test_get(self):
+        self.fail()
---
+++ tests/test_parsers.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestParsers(TestCase):
+    def test_digit(self):
+        self.fail()
+
+    def test_single_whitespace(self):
+        self.fail()
+
+    def test_all_whitespace(self):
+        self.fail()
+
+    def test_parse_int(self):
+        self.fail()
